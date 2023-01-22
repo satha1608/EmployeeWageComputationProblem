bde86d133a89d8b61e758f18a819660a7934525a
“satha1608” <sathasivam1608@gmail.com>
2023-01-23 03:36:23 +0530

    Refactor: Refactor the Code to write a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,19 +8,21 @@ numWorkingDays=20
 totalEmpHr=0
 totalWorkingDays=0
 
+function getWorkingHours() {
+ case $1 in $isFullTime)
+                        empHrs=8;;
+                $isPartTime)
+                        empHrs=4;;
+                *)
+                        empHrs=0;;
+        esac
+     echo $empHrs
+}
 while [[ $totalEmpHr -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
 do
-	((totalWorkingDays++))
-	empCheck=$((RANDOM%3))
-		case $empCheck in $isFullTime)
-			empHrs=8;;
-		$isPartTime)
-			empHrs=4;;
-		*)
-			empHrs=0;;
-	esac
-	totalEmpHr=$(($totalEmpHr+$empHrs))
+((totalWorkingDays++))
+empHrs=$(getWorkingHours $((RANDOM%3)))
+totalEmpHr=$(($totalEmpHr+$empHrs))
 done
 totalSalary=$(($totalEmpHr*$empRatePerHr))
-
 echo "Employee has earned Rs.$totalSalary salary in a month ( Total working hour : $totalEmpHr )"
