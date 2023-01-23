cd6213dc1268900814ac63227dcc7719c98e3a8d
“satha1608” <sathasivam1608@gmail.com>
2023-01-23 18:54:45 +0530

    Add: Store the Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,28 +1,35 @@
-echo "Welcome to Employee Wage Computation Program"
+	echo "Welcome to Employee Wage Computation Program"
 
-isPartTime=1
-isFullTime=2
-maxHrsInMonth=10
-empRatePerHr=20
-numWorkingDays=20
-totalEmpHr=0
-totalWorkingDays=0
+	isPartTime=1
+	isFullTime=2
+	maxHrsInMonth=10
+	empRatePerHr=20
+	numWorkingDays=20
+	totalEmpHr=0
+	totalWorkingDays=0
 
-function getWorkingHours() {
- case $1 in $isFullTime)
-                        empHrs=8;;
-                $isPartTime)
-                        empHrs=4;;
-                *)
-                        empHrs=0;;
-        esac
-     echo $empHrs
-}
-while [[ $totalEmpHr -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
-do
-((totalWorkingDays++))
-empHrs=$(getWorkingHours $((RANDOM%3)))
-totalEmpHr=$(($totalEmpHr+$empHrs))
-done
-totalSalary=$(($totalEmpHr*$empRatePerHr))
-echo "Employee has earned Rs.$totalSalary salary in a month ( Total working hour : $totalEmpHr )"
+	function getWorkingHours() {
+	 case $1 in $isFullTime)
+	                        workHours=8;;
+	                $isPartTime)
+	                        workHours=4;;
+	                *)
+	                        workHours=0;;
+	        esac
+	     echo $workHours
+	}
+	function calcDailyWage() {
+	 local worker=$1
+	wage=$(($worker*$empRatePerHr))
+	echo $wage
+	}
+		while [[ $totalEmpHr -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+		do
+		((totalWorkingDays++))
+		workHours=$(getWorkingHours $((RANDOM%3)))
+	totalEmpHr=$(($totalEmpHr+$workHours))
+	empDailyWage[$totalWorkingDays]=$(calcDailyWage $(($workHours))) 
+	done
+	totalSalary=$(calcDailyWage $(($totalEmpHr))) 
+	echo "Daily Wage " ${empDailyWage[@]}
+	echo "Employee has earned Rs.$totalSalary salary in a month ( Total working hour : $totalEmpHr )"
